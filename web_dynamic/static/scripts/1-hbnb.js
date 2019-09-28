73ca7778a628552096c2d8034e5665ce2e3b57ba
Tim Assavarat <tassavarat@gmail.com>
2019-09-28 05:12:36 +0000

    Implemented checkbox functionality and fixed html syntax

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,5 +1,11 @@
 $(() => {
-  $('input').click(() => {
-    alert('checked');
+  const aDict = {};
+
+  $('input').change(() => {
+    for (const idx of $('input')) {
+      if (idx.checked) aDict[idx.dataset.name] = idx.dataset.id;
+      else delete aDict[idx.dataset.name];
+    }
+    $('.amenities h4').html(Object.keys(aDict));
   });
 });
