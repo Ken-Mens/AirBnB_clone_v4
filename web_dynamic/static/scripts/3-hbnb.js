ee75dba4571fe1a9a6ba71c0200535f9ffbb628d
Tim Assavarat <tassavarat@gmail.com>
2019-09-29 01:47:42 +0000

    Update skeleton for loop

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -19,10 +19,28 @@ $.get('http://0.0.0.0:5001/api/v1/places_search/', function (data, status) {
   }
 });
 
+const dObj = {};
+
 $.ajax({
   type: 'POST',
   url: 'http://0.0.0.0:5001/api/v1/places_search/',
   contentType: 'application/json',
-  data: '{}'
+  data: JSON.stringify(dObj),
+  success: function (data) {
+    $('.places h1').after('<article>');
+    $('article').append('<div class="title">');
+    $('.title').append('<h2>').append('<div class="price_by_night">');
+    $('.title').after('<div class="information">');
+    $('.information').append('<div class="max_guest">');
+    $('.max_guest').append('<i class="fa fa-users fa-3x" aria-hidden="true">'); // Has before
+    $('.fa.fa-users.fa-3x').after('<br />');
+    $('.max_guest').after('<div class="number_rooms">'); // Has before
+    $('.number_rooms').append('<i class="fa fa-bed fa-3x" aria-hidden="true">'); // Has before
+    $('.fa.fa-bed.fa-3x').after('<br />');
+    $('.number_rooms').after('<div class="number_bathrooms">');
+    $('.number_bathrooms').append('<i class="fa fa-bath fa-3x" aria-hidden="true">'); // Has before
+    $('.fa.fa-bath.fa-3x').after('<br />');
+    $('.information').after('<div class="user">');
+    $('.user').after('<div class="description">');
+  }
 });
-console.log(data);
